f440f5ec8b43f9cb0d675f3eb7faac6323f81bf5
Mustafa SEYIP <mseyip03@gmail.com>
2024-03-29 10:12:12 +0300

    Easy-5

---
+++ src/main/java/easy_level/Loops_1.java
@@ -0,0 +1,55 @@
+package easy_level;
+
+import java.io.*;
+
+public class Loops_1 {
+
+    /*
+    Objective
+In this challenge, we're going to use loops to help us do some simple math.
+
+Task
+Given an integer, N, print its first 10 multiples. Each multiple Nxi (where 1<=i<=10) should be printed on a new line in the form: N x i = result.
+
+Input Format
+
+A single integer, N.
+
+Constraints
+2<=N<=20
+Output Format
+
+Print 10 lines of output; each line i (where 1<=i<=10) contains the result of Nxi in the form:
+N x i = result.
+
+Sample Input
+
+2
+Sample Output
+
+2 x 1 = 2
+2 x 2 = 4
+2 x 3 = 6
+2 x 4 = 8
+2 x 5 = 10
+2 x 6 = 12
+2 x 7 = 14
+2 x 8 = 16
+2 x 9 = 18
+2 x 10 = 20
+     */
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+
+        for (int i = 1; i <= 10; i++) {
+            int result = N * i;
+            System.out.printf("%d x %d = %d%n", N, i, result);
+
+        }
+
+        bufferedReader.close();
+    }
+}
